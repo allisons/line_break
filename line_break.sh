25ccbb0afc399887813a385ceab1f8b372dc5276
Allison Sliter <allison.sliter@gmail.com>
2015-06-29 09:25:27 -0700

    Update line_break.sh

--- line_break.sh
+++ line_break.sh
@@ -2,12 +2,10 @@
 FOLDER=$1
 MODEL=$2
 TEMPLATE=$3
-GOLD=$4
-CHAR=$5
 
 python training_and_testing_data_prep.py $FOLDER
 crf_learn $TEMPLATE train_features $MODEL
 python test_shell_generator.py $MODEL > tester.sh
 ./tester.sh
-python table_to_txt.py formatted_output_data $GOLD $CHAR
+python table_to_txt.py formatted_output_data 1 0
 
