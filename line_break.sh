891a716e1db1c54903d4b69fd3a67843dff387f0
allisons <allison.sliter@gmail.com>
2015-07-27 22:16:24 -0700

    Wait, now it should work

--- line_break.sh
+++ line_break.sh
@@ -8,8 +8,8 @@ GOLD=$6
 REBUILD=$7
 
 python training_and_testing_data_prep.py $TRAINFILE $TESTFILE $FEATSET
-#crf_learn $TEMPLATE train_features $MODEL
-#python test_shell_generator.py $MODEL formatted_test_data > tester.sh
-#./tester.sh
-#python table_to_txt.py formatted_output_data $GOLD $TEMPLATE $REBUILD
+crf_learn $TEMPLATE train_features $MODEL
+python test_shell_generator.py $MODEL formatted_test_data > tester.sh
+./tester.sh
+python table_to_txt.py formatted_output_data $GOLD $TEMPLATE $REBUILD
 
